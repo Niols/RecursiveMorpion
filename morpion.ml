ded5e743d684b1851cd1e98b0a60b6d492405948
Nicolas Jeannerod <nicojpub@gmail.com>
2014-11-16 12:57:04 +0100

    Added create functions and setters

--- morpion.ml
+++ morpion.ml
@@ -13,7 +13,7 @@
 
 type status = Free | Cross | Circle | Tie
 
-type 'c morpion = 'c array
+type 'c morpion = 'c array * ref status
 
 type rmorpion = status morpion morpion
 
@@ -21,14 +21,16 @@ type location = int * int
 
 type rlocation = location * location
 
-
 let loc_to_i (x, y) =
-  
+  (x - 1) + 3 * (y - 1)
 
+let i_to_loc i =
+  ( 1 + (i mod 3) ,
+    1 + (i / 3)   )
 
 exception Error of string
 
-let get m l =
+let get (m, s) l =
   try
     m.(loc_to_i l)
   with
@@ -37,3 +39,36 @@ let get m l =
 
 let rget rm (l1, l2) =
   get (get rm l1) l2
+
+let set (m, s) l c =
+  try
+    m.(loc_to_i l) <- c
+  with
+  | Invalid_argument "index out of bounds" ->
+    raise (Error "bad location")
+
+let rset rm (l1, l2) s =
+  set (get rm l1) l2 s
+
+let gets (_, rs) =
+  !rs
+
+let rgets rm l =
+  gets (get rm l)
+
+let sets (_, rs) s =
+  rs := s
+
+let rsets rm l s =
+  sets (get rm l) s
+
+let create c =
+  ( Array.make 9 c , Free )
+
+let rcreate () =
+  let rm = create (create Free) in
+  for i = 1 to 9 - 1 do
+    set rm (i_to_loc i) (create Free)
+  done;
+  rm
+
--- morpion.mli
+++ morpion.mli
@@ -2,6 +2,7 @@
 (*                                                                            *)
 (*                              Recursive Morpion                             *)
 (*                                   Morpion                                  *)
+(*                                  Signature                                 *)
 (*                                                                            *)
 (*   THE BEER-WARE LICENSE (Revision 42): Niols wrote this file.              *)
 (*   As long as you retain this notice you can do whatever you want with      *)
@@ -18,7 +19,7 @@ type status = Free | Cross | Circle | Tie
    The different cell status possible.
 *)
 
-type 'c morpion
+type 'c morpion = 'c array * ref status
 (**
    The type of the morpion.
 *)
@@ -31,13 +32,20 @@ type rmorpion = status morpion morpion
 type location = int * int
 (**
    The type of a location in the morpion.
+   A [location] is an element of {1,2,3}².
 *)
 
 type rlocation = location * location
 (**
    The type of a location in the recursive morpion.
+   A [rlocation] is an element of {1,2,3}²².
    If [(loc1, loc2)] has type [rlocation], then loc1 is the position in the
    big morpion, and loc2 in the little morpion in big morpion's cell.
+*)
+
+exception Error of string
+(**
+   
 *)
 
 val get : 'c morpion -> location -> 'c
@@ -62,3 +70,32 @@ val rset : rmorpion -> rlocation -> status -> unit
    rlocation [rl] to status [s].
 *)
 
+val gets : 'c morpion -> status
+(**
+   [gets m] returns the status of the morpion [m].
+*)
+
+val rgets : status morpion morpion -> location -> status
+(**
+   [rgets rm l] returns the status of the submorpion at location [l].
+*)
+
+val sets : 'c morpion -> status -> unit
+(**
+   [sets m s] sets the status of the morpion [m] to [s].
+*)
+
+val rsets : 'c morpion -> location -> status -> unit
+(**
+   [rsets rm l s] sets the status of the submorpion at location [l] to [s].
+*)
+
+val create : 'c -> 'c morpion
+(**
+   [create c] creates a morpion filled with [c].
+*)
+
+val rcreate : unit -> status morpion morpion
+(**
+   Creates an empty recursive morpion.
+*)
